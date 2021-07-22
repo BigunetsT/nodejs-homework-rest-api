c6c24d31fa28d9f8ec107d75f9c6d20be9388127
BigunetsT <tanyuschamail83@gmail.com>
2021-07-22 08:44:24 +0300

    fix magic numbers

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const logger = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const rateLimit = require('express-rate-limit')
+const { apiLimit, jsonLimit } = require('./config/rate-limit.json')
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
+  windowMs: apiLimit.windowMs, // 15 * 60 * 1000 = 15 minutes
+  max: apiLimit.max,
 })
 
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json({ limit: 10000 }))
+app.use(express.json({ limit: jsonLimit }))
 app.use(helmet())
 app.use('/api/', apiLimiter)
 
